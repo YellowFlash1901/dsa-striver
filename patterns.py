83cf7c75a76f499e1054fc8b3d81e343acc10a83
YellowFlash1901 <vardaansathe@gmail.com>
2025-09-22 20:40:25 +0100

    patterns started

---
+++ patterns.py
@@ -0,0 +1,13 @@
+
+# for i in range(0,nums):
+#     for j in range(0,nums):
+#         print("*", end = " ")
+#     print()
+
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+
+
